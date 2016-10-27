ff263465b6a3e194ca9271ed3ed397a6ac338f6e
experion <you@example.com>
2016-10-27 18:30:52 +0530

    initial commit

---
+++ factorial.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class factorial
+{
+ public static void main()throws IOException
+ {
+   BufferReader br=new BufferReader();
+   InputStreamReader isr=new InputStreamReader();
+   int i,n,k;
+   
+     for(j=i;j<=n;j++)
+      {
+        k=i*j;
+       
+       }
+System.out.pritln(k);
+    }
+  }
+
